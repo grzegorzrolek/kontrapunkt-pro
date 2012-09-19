f5c616a8a77b418ba6e83241ebb8cadcbeb833ec
Grzegorz Rolek <ichosis@gmail.com>
2012-09-20 01:09:17 +0200

    Kickoff with raw dumps of the original

---
+++ build.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+# Build script for Kontrapunkt Pro project directory
+# Copyright 2012 Grzegorz Rolek
+#
+# This script builds Kontrapunkt Pro OpenType binaries. It makes a readable commentary
+# on the whole build process, but for convenience just use 'make' instead.
+# Pass into the script any of the 'makeotf' options to extend or override those below.
+
+
+# Check for critical tools.
+for TOOL in t1unmac t1disasm t1asm t1reencode mergeFonts rotateFont makeotf
+do type $TOOL &>/dev/null || { echo >&2 "Fatal: No '$TOOL' found; see README for requirements."; exit 1; }
+done
+
+# Clean up existing builds, if any.
+rm -rf build && mkdir build
+
+# Dump an empty PostScript encoding vector for use in the merge process.
+# Type 1 Reencode could take it as an argument, but apparently fails on doing so.
+test -f empty.enc || printf '%s\n' '/Empty [' $(for SLOT in {1..256}
+	do echo '/.notdef'
+	done) '] def' >empty.enc
+
+# Iterate through subdirectories of the family styles.
+for STYLE in Light LightItalic Bold
+do
+
+	# Figure out short style name according to the "5:3:3" rule.
+	STY=$(sed 's/\([A-Z][a-z]\{0,2\}\)[a-z]*/\1/g' <<<$STYLE)
+
+	# Paths to base Kontrapunkt family and its derivatives without the various suffixes.
+	BASE=Kontrapunkt/$STYLE/Kontr$STY
+	CE=KontrapunktCE/$STYLE/KontrCE$STY
+	EXPERT=KontrapunktExpert/$STYLE/KontrExp$STY
+
+
+	# PREPARING THE BASE FONT & KERNING DATA
+
+	# Those Mac-specific fonts store font programs in POST resource forks.
+	# They were archived with resource-savvy 'ditto' utility for repository storage.
+	test -f $BASE || ditto -x $BASE.cpgz $(dirname $BASE)
+
+	# Encode the forks and dump a raw PostScript Type 1 source for further processing.
+	test -f $BASE.ps || macbinary encode -p $BASE | t1unmac -a | t1disasm >$BASE.ps
+
+	# There's apparently no kerning listing in the original ASCII metrics, nor in the FOND resources.
+	# Still, there's one in the printer binaries, so take it out and write into a new ASCII metrics file.
+	if ! test -f ${BASE}Kern.afm
+	then
+
+		# Byte offset to the kerning table is stored as four bytes at byte 131 in a PFM binary.
+		# PFM binaries have a little-endian byte order; be cautious on big-endian architectures.
+		# See Adobe's Technical Note #5178, Building PFM files... for a full spec.
+		OFFSET=$(hexdump -s 131 -n 4 -e '"%u"' $BASE.pfm)
+
+		# Number of kern pair entries: first 2 bytes of the table.
+		NO=$(hexdump -s $OFFSET -n 2 -e '/2 "%u"' $BASE.pfm)
+
+		# Dump the entries as newline-separated "char char value" sequences.
+		# Entries start at 2 bytes into the table, 4 bytes long each (two chars plus a signed short).
+		PAIRS=$(hexdump -s $(($OFFSET + 2)) -n $(($NO * 4)) -e '2/1 "%u " /2 " %i\n"' $BASE.pfm)
+
+		# Parse the related encoding file for a bare list of glyphs names.
+		NAMES=($(sed -n 's/^\/\(.*[^ []\)$/\1/p' <default.enc))
+
+		# Prepare proper kerning listing with a header/trailer and the entries by glyph name.
+		KERN=$(
+			echo "StartKernData"
+			echo "StartKernPairs $NO"
+			while read PAIR
+			do SEQ=($PAIR); echo "KPX ${NAMES[${SEQ[0]}]} ${NAMES[${SEQ[1]}]} ${SEQ[2]}"
+			done <<<"$PAIRS"
+			echo "EndKernPairs"
+			echo "EndKernData"
+		)
+
+		# Duplicate the original ASCII metrics into a new file with newlines instead of carriage returns.
+		# Note the locale change; this avoids 'tr' failure on some non-ASCII chars in the file.
+		LC_CTYPE=C; tr '\r' '\n' <$BASE.afm >${BASE}Kern.afm
+
+		# Insert the listing into the new metrics just after the EndCharMetrics line.
+		printf '%s\n' /EndCharMetrics/+1i "$KERN" . wq | ed -s ${BASE}Kern.afm
+
+	fi
+
+
+	# MERGING THE EXTENDED CHARSET WITH THE BASE FAMILY
+
+	# Because 'mergeFonts' spits an error for each and every duplicate glyph it encounters in a merge queue,
+	# make a merge map for each family with a diff of new glyphs the family introduces.
+	# This also makes it possible to use a pre-prepared map and exclude any glyph of the original family,
+	# thus letting its duplicate in derived family to be merged in instead.
+
+	# Start making the diffs with a list of glyphs present in the Kontrapunkt Pro font file.
+	SET=$(sed -n '/^\/\(..*\) {$/s//\1/p' $STYLE/font.ps)
+
+	for FAMILY in base ce expert
+	do
+
+		# Take out an actual path to the font of this particular family.
+		eval FONT=\$$(echo $FAMILY | tr 'a-z' 'A-Z')
+
+		# If there's no map for this family or if it's outdated, then make one with all the new glyphs the family provides.
+		if ! test -f $STYLE/$FAMILY.map || test -f $STYLE/$FAMILY.list && test $STYLE/$FAMILY.map -ot $FONT.ps
+		then
+
+			# Extract and dump a glyph list from the font for comparison.
+			sed -n '/^\/\(..*\) {$/s//\1/p' $FONT.ps | sort -u >$STYLE/$FAMILY.list
+
+			# Filter glyphs that haven't been introduced already and dump a merge map with a proper header.
+			printf '%s\n' 'mergeFonts' $(sort -u <<<"$SET" | comm -13 - $STYLE/$FAMILY.list) | sed '2,$s/^.*$/& &/' >$STYLE/$FAMILY.map
+
+		fi
+
+		# Extend the list of glyphs already included with those in the family's map.
+		SET=$(echo "$SET"; sed -e '/^mergeFonts$/d' -e '/^#.*$/d' -e 's/^\(..*\) \1$/\1/' $STYLE/$FAMILY.map)
+
+		# Build the font from its PostScript sources and null out the encoding to avoid conflicts on merge.
+		t1asm -a $FONT.ps | t1reencode -a -e empty.enc >$STYLE/$FAMILY.pfa
+
+	done
+
+	# Build an empty Kontrapunkt Pro Type 1 ASCII font file.
+	t1asm -a $STYLE/font.ps $STYLE/font.pfa
+
+	# Merge each of the basic families into the Kontrapunkt Pro font file.
+	mergeFonts $STYLE/font.pfa $STYLE/font.pfa $(
+
+		# Make a family into the queue only if there's anything in particular to merge.
+		# This is necessery, because in case of an empty map the font's whole contents is merged.
+		for FAMILY in base ce expert
+		do test $(wc -w <$STYLE/$FAMILY.map) -gt 1 && echo $STYLE/$FAMILY.map $STYLE/$FAMILY.pfa
+		done
+
+	)
+
+
+	# POST-PROCESSING
+
+	# Shift glyphs in their em-boxes or change their advance widths if necessary.
+	test -f $STYLE/shift.map && rotateFont -t1 -rtf $STYLE/shift.map $STYLE/font.pfa $STYLE/font.pfa
+
+	# Base the PostScript revision number on unique commit count of the source files involved.
+	PSREV=$(printf '%03d' $(git rev-list HEAD -- $STYLE/font.ps $CE.ps $EXPERT.ps $STYLE/*.map | wc -l))
+
+	# Figure out pretty format of style name for a dedicated PostScript entry.
+	FULL_STYLE=$(sed 's/\([a-z][a-z]*\)\([A-Z][A-Z]*\)/\1 \2/g' <<<$STYLE)
+
+	# Make the processing.
+	ed -s $STYLE/font.pfa <<<'H
+		/^\(%!FontType1-1.1: \)..*\( [0-9]\{3\}\.\)[0-9]\{3\}$/s//\1KontrapunktPro-'$STYLE'\2'$PSREV'/
+		/^\(\/FontName \/\)..*\( def\)$/s//\1KontrapunktPro-'$STYLE'\2/
+		/^\(\/version ([0-9]\{3\}\.\)[0-9]\{3\}\()\( readonly\)\{0,1\} def\)$/s//\1'$PSREV'\2/
+		/^\(\/FullName (\)..*\()\( readonly\)\{0,1\} def\)$/s//\1Kontrapunkt Pro '$FULL_STYLE'\2/
+		/^\(\/FamilyName (\)..*\()\( readonly\)\{0,1\} def\)$/s//\1Kontrapunkt Pro\2/
+		wq'
+
+
+	# OPENTYPE COMPILATION
+
+	# Merge and translate kerning listing from AFM to OpenType syntax.
+	sort -u ${BASE}Kern.afm $CE.afm $EXPERT.afm | sed -n 's/^KPX \(.*$\)/pos \1;/p' >$STYLE/features.kern
+
+	# Make the font revision number with appropriate commit count and dump a snippet for inclusion in the features file.
+	REV=$(printf '%03d' $(git rev-list HEAD -- FontMenuNameDB GlyphOrderAndAliasDB $STYLE/features features.family $STYLE/fontinfo | wc -l))
+	echo "table head { FontRevision 1.$REV; } head;" >$STYLE/fontrev
+
+	# Make the OpenType binaries with all prepared files.
+	makeotf -ga -f $STYLE/font.pfa -o build/KontrapunktPro-$STYLE.otf $@
+
+done
+
+exit
---
+++ makefile
@@ -0,0 +1,172 @@
+# Makefile for Kontrapunkt Pro project directory
+# Copyright 2012 Grzegorz Rolek
+#
+# Use MAKEOTF_OPTIONS to pass custom options into 'makeotf' compiler.
+# See build.sh for a commentary on the build process details.
+
+
+STYLES = Light LightItalic Bold
+STYS = Lig LigIta Bol
+
+# Kontrapunkt Pro OpenType binaries.
+OTF = $(foreach STYLE,$(STYLES),build/KontrapunktPro-$(STYLE).otf)
+
+# Paths to Kontrapunkt Pro Type 1 families minus the suffixes.
+PRO = $(addsuffix /font,$(STYLES))
+
+# Paths to basic families prepared for merging minus the suffixes.
+MERGE = $(foreach STYLE,$(STYLES),$(STYLE)/base $(STYLE)/ce $(STYLE)/expert)
+
+# Paths to base Kontrapunkt and derived families minus the various suffixes.
+EXPERT = $(addprefix KontrapunktExpert/,$(join $(STYLES),$(foreach STY,$(STYS),/KontrExp$(STY))))
+CE = $(addprefix KontrapunktCE/,$(join $(STYLES),$(foreach STY,$(STYS),/KontrCE$(STY))))
+BASE = $(subst CE,,$(CE))
+
+# Normally, in static pattern rules with a filter function, it's the filter who interprets the % wildcard.
+# Secondary expansion makes things tricky, though, and it seems that's no longer the case.
+# The variable below escaped twice makes sure the % wildcard indeed goes into the filter itself.
+WLDCRD = %
+
+
+# KONTRAPUNKT PRO OPENTYPE BINARIES
+
+all: $(OTF)
+
+$(OTF): build/KontrapunktPro-%.otf: %/font.pfa %/features features.family FontMenuNameDB GlyphOrderAndAliasDB %/fontinfo | %/features.kern %/fontrev build
+	makeotf -ga -f $< -o $@ $(MAKEOTF_OPTIONS)
+
+$(addsuffix /fontrev,$(STYLES)): %/fontrev: FontMenuNameDB GlyphOrderAndAliasDB %/features features.family %/fontinfo
+	REV=$$(printf '%03d' $$(git rev-list HEAD -- \
+		FontMenuNameDB GlyphOrderAndAliasDB $*/features features.family $*/fontinfo | wc -l)); \
+	echo "table head { FontRevision 1.$$REV; } head;" >$@
+
+.SECONDEXPANSION:
+$(addsuffix /features.kern,$(STYLES)): %/features.kern: $$(filter Kontrapunkt/$$*/$$(WLDCRD),$$(BASE))Kern.afm
+	sort -u $< $(filter KontrapunktCE/$*/%,$(CE)).afm $(filter KontrapunktExpert/$*/%,$(EXPERT)).afm | \
+	sed -n 's/^KPX \(.*$$\)/pos \1;/p' >$@
+
+$(addsuffix Kern.afm,$(BASE)): %Kern.afm: %.pfm default.enc %.afm
+	OFFSET=$$(hexdump -s 131 -n 4 -e '"%u"' $*.pfm); \
+	NO=$$(hexdump -s $$OFFSET -n 2 -e '/2 "%u"' $*.pfm); \
+	PAIRS=$$(hexdump -s $$(($$OFFSET + 2)) -n $$(($$NO * 4)) -e '2/1 "%u " /2 " %i\n"' $*.pfm); \
+	NAMES=($$(sed -n 's/^\/\(.*[^ []\)$$/\1/p' <default.enc)); \
+	LC_CTYPE=C; tr '\r' '\n' <$*.afm >$@; \
+	KERN=$$( \
+		echo "StartKernData"; \
+		echo "StartKernPairs $$NO"; \
+		while read PAIR; \
+		do SEQ=($$PAIR); echo "KPX $${NAMES[$${SEQ[0]}]} $${NAMES[$${SEQ[1]}]} $${SEQ[2]}"; \
+		done <<<"$$PAIRS"; \
+		echo "EndKernPairs"; \
+		echo "EndKernData" \
+	); \
+	printf '%s\n' /EndCharMetrics/+1i "$$KERN" . wq | ed -s $@
+
+build:
+	mkdir build
+
+
+# KONTRAPUNKT PRO TYPE 1 FAMILY
+
+pro: $(addsuffix .pfa,$(PRO))
+
+.SECONDEXPANSION:
+.ONESHELL:
+$(addsuffix .pfa,$(PRO)): %/font.pfa: %/font.ps $$(addsuffix .map,$$*/base $$*/ce $$*/expert) $$(addsuffix .pfa,$$*/base $$*/ce $$*/expert) $$(wildcard $$*/shift.map)
+	t1asm -a $*/font.ps $@
+	mergeFonts $@ $@ $$(for FAMILY in base ce expert; do test $$(wc -w <$*/$$FAMILY.map) -gt 1 && echo $*/$$FAMILY.map $*/$$FAMILY.pfa; done)
+	-test -f $*/shift.map && rotateFont -t1 -rtf $*/shift.map $@ $@
+	PSREV=$$(printf '%03d' $$(git rev-list HEAD -- \
+		$*/font.ps $(filter KontrapunktCE/$*/%,$(CE)).ps $(filter KontrapunktExpert/$*/%,$(EXPERT)).ps $*/*.map | wc -l)); \
+	FULL_STYLE=$$(sed 's/\([a-z][a-z]*\)\([A-Z][A-Z]*\)/\1 \2/g' <<<$*); \
+	printf '%s\n' \
+		"/^\(%!FontType1-1.1: \)..*\( [0-9]\{3\}\.\)[0-9]\{3\}$$/s//\1KontrapunktPro-$*\2$$PSREV/" \
+		"/^\(\/FontName \/\)..*\( def\)$$/s//\1KontrapunktPro-$*\2/" \
+		"/^\(\/version ([0-9]\{3\}\.\)[0-9]\{3\}\()\( readonly\)\{0,1\} def\)$$/s//\1$$PSREV\2/" \
+		"/^\(\/FullName (\)..*\()\( readonly\)\{0,1\} def\)$$/s//\1Kontrapunkt Pro $$FULL_STYLE\2/" \
+		"/^\(\/FamilyName (\)..*\()\( readonly\)\{0,1\} def\)$$/s//\1Kontrapunkt Pro\2/" \
+		wq | ed -s $@
+
+.SECONDEXPANSION:
+$(addsuffix .pfa,$(filter %expert,$(MERGE))): %/expert.pfa: $$(filter KontrapunktExpert/$$*/$$(WLDCRD),$$(EXPERT)).ps | empty.enc
+	t1asm -a $< | t1reencode -a -e empty.enc >$@
+
+.SECONDEXPANSION:
+$(addsuffix .pfa,$(filter %ce,$(MERGE))): %/ce.pfa: $$(filter KontrapunktCE/$$*/$$(WLDCRD),$$(CE)).ps | empty.enc
+	t1asm -a $< | t1reencode -a -e empty.enc >$@
+
+.SECONDEXPANSION:
+$(addsuffix .pfa,$(filter %base,$(MERGE))): %/base.pfa: $$(filter Kontrapunkt/$$*/$$(WLDCRD),$$(BASE)).ps | empty.enc
+	t1asm -a $< | t1reencode -a -e empty.enc >$@
+
+empty.enc:
+	printf '%s\n' '/Empty [' $(for SLOT in {1..256}; do echo '/.notdef'; done) '] def' >$@
+
+.SECONDEXPANSION:
+$(foreach STYLE,$(STYLES),$(STYLE)/expert.map): %/expert.map: $$(filter KontrapunktExpert/$$*/$$(WLDCRD),$$(EXPERT)).ps %/font.ps %/base.map %/ce.map
+	sed -n '/^\/\(..*\) {$$/s//\1/p' $< | sort -u >$*/expert.list
+	SET=$$(sed -n '/^\/\(..*\) {$$/s//\1/p' $*/font.ps; \
+	sed -e '/^mergeFonts$$/d' -e '/^#.*$$/d' -e 's/^\(..*\) \1$$/\1/' $*/base.map $*/ce.map); \
+	printf '%s\n' 'mergeFonts' $$(sort -u <<<"$$SET" | comm -13 - $*/expert.list) | sed '2,$$s/^.*$$/& &/' >$@
+
+.SECONDEXPANSION:
+$(foreach STYLE,$(STYLES),$(STYLE)/ce.map): %/ce.map: $$(filter KontrapunktCE/$$*/$$(WLDCRD),$$(CE)).ps %/font.ps %/base.map
+	sed -n '/^\/\(..*\) {$$/s//\1/p' $< | sort -u >$*/ce.list
+	SET=$$(sed -n '/^\/\(..*\) {$$/s//\1/p' $*/font.ps; \
+	sed -e '/^mergeFonts$$/d' -e '/^#.*$$/d' -e 's/^\(..*\) \1$$/\1/' $*/base.map); \
+	printf '%s\n' 'mergeFonts' $$(sort -u <<<"$$SET" | comm -13 - $*/ce.list) | sed '2,$$s/^.*$$/& &/' >$@
+
+.SECONDEXPANSION:
+$(foreach STYLE,$(STYLES),$(STYLE)/base.map): %/base.map: %/font.ps | $$(filter Kontrapunkt/$$*/$$(WLDCRD),$$(BASE)).ps 
+	sed -n '/^\/\(..*\) {$$/s//\1/p' $| | sort -u >$*/base.list
+	SET=$$(sed -n '/^\/\(..*\) {$$/s//\1/p' $*/font.ps); \
+	printf '%s\n' 'mergeFonts' $$(sort -u <<<"$$SET" | comm -13 - $*/base.list) | sed '2,$$s/^.*$$/& &/' >$@
+
+$(addsuffix .ps,$(BASE)): %.ps: %
+	macbinary encode -p $< | t1unmac -a | t1disasm >$@
+
+
+# BASE KONTRAPUNKT TYPE 1 FAMILY & DERIVATIVES
+
+base: $(addsuffix .pfa,$(BASE))
+ce: $(addsuffix .pfa,$(CE))
+expert: $(addsuffix .pfa,$(EXPERT))
+
+$(addsuffix .pfa,$(CE) $(EXPERT)): %.pfa: %.ps
+	t1asm -a $< $@
+
+$(addsuffix .pfa,$(BASE)): %.pfa: %
+	macbinary encode -p $< | t1unmac -a >$@
+
+# This rule extracts files from archive with file modification dates older then the archive itself.
+# It would trigger the recipe each and every time, unless the order-only prerequisite is used.
+$(BASE): %: | %.cpgz
+	ditto -x $| $(dir $@)
+
+
+# TESTING
+
+test: $(OTF)
+	@cd build; compareFamily; cd ..
+
+lint: $(addsuffix .pfa,$(CE) $(EXPERT) $(PRO))
+	-@t1lint $(addsuffix .pfa,$(CE) $(EXPERT) $(PRO))
+
+check: $(addsuffix .pfa,$(CE) $(EXPERT))
+	@rm -f $(addsuffix .log,$(CE) $(EXPERT))
+	-@for FONT in $(CE) $(EXPERT); do \
+	echo $$FONT.pfa; \
+	checkOutlines -i -log $$FONT.log $$FONT.pfa | grep 'Warning:'; \
+	done
+
+
+# CLEANING
+
+clean:
+	@rm -fr $(BASE) $(addsuffix .ps,$(BASE)) \
+		$(addsuffix .pfa,$(BASE) $(CE) $(EXPERT) $(PRO)) \
+		empty.enc $(addsuffix .pfa,$(MERGE)) $(addsuffix .list,$(MERGE)) $(addsuffix .map,$(MERGE)) \
+		$(addsuffix Kern.afm,$(BASE)) $(addsuffix /features.kern,$(STYLES)) \
+		$(addsuffix /fontrev,$(STYLES)) \
+		$(addsuffix /current.fpr,$(STYLES)) build \
+		$(addsuffix .log,$(CE) $(EXPERT))
